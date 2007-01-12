4baa9e4b2530e8d4c2f0adec619e225e94b5770d
rgigger <rgigger@585e9b84-f019-0410-b165-1d737e75257b>
2007-01-12 07:34:20 +0000

    several changes: 1) fixed the Hello World app to work out of the box, got rid of a path specific to my install, etc 2) Changed everything Pehppy to Zoop 3) started the new todo application
    git-svn-id: https://zoop.googlecode.com/svn/trunk@13 585e9b84-f019-0410-b165-1d737e75257b

---
+++ framework/Zoop.php
@@ -0,0 +1,44 @@
+<?php
+//	we rely on this all over the place.  there is no point in trying to do
+//	anything until we've got this assigned
+if(!defined('zoop_dir'))
+	trigger_error('Please define the constant "zoop_dir" before continuing.  It should point to the base directory of the zoop framwork');
+
+class Zoop
+{
+	function loadLib($name)
+	{
+		//	allow static calls to use the singleton object
+		if(!isset($this))
+		{
+			global $zoop;
+			$zoop->loadLib($name);
+			return;
+		}
+		
+		switch($name)
+		{
+			case 'app':
+				$this->loadLib('utils');
+				include(zoop_dir . '/app/Globals.php');
+				include(zoop_dir . '/app/Application.php');
+				break;
+			case 'utils':
+				include(zoop_dir . '/utils/Utils.php');
+				break;
+			case 'gui':
+				include(zoop_dir . "/smarty/Smarty.class.php");
+				include(zoop_dir . '/gui/gui.php');
+				break;
+			case 'zone':
+				$this->loadLib('app');
+				include(zoop_dir . '/zone/Zone.php');
+				include(zoop_dir . '/zone/ZoneApplication.php');
+				break;
+			default:	
+				trigger_error('unknown module: ' . $name);
+		}
+	}
+}
+
+$zoop = new Zoop();
