b664e3476b39eb484164658c7598cf67a26366a8
rgigger <rgigger@585e9b84-f019-0410-b165-1d737e75257b>
2007-03-07 22:45:27 +0000

    updates to dbobject
    git-svn-id: https://zoop.googlecode.com/svn/trunk@103 585e9b84-f019-0410-b165-1d737e75257b

--- framework/db/DbObject.php
+++ framework/db/DbObject.php
@@ -4,11 +4,20 @@ class DbObject
 	var $id;
 	var $scalars;
 	var $hasMany;
+	var $autoSave;
 	
-	function DbObject($init = NULL)
+	function DbObject($init = NULL, $defaults = NULL)
 	{
-		if(!$init)
+		//	terrible terrible hack.  Alas if only we had late static binding
+		if($init == 'static')
 			return;
+		
+		if($defaults)
+		{
+			assert(is_array($defaults));
+			if(!is_null($init))
+				trigger_error("defaults can only be used when creating the row in the database.  use setscalars to set them after it is created");
+		}
 		
 		switch(gettype($init))
 		{
@@ -29,12 +38,24 @@ class DbObject
 					trigger_error('not yet implemented');
 				}
 				break;
+			case 'NULL':
+				//	we just need to create a new blank object
+				$tableName = $this->getTableName();
+				if(!$defaults)
+					$this->id = SqlInsertRow("insert into $tableName default values", array());
+				else
+				{
+					$info = DbConnection::generateInsertInfo($tableName, $defaults);
+					$this->id = SqlInsertRow($info['sql'], $info['params']);
+				}
+				break;
 			default:
 				trigger_error('object not initialized');
 				break;
 		}
 		
 		$this->hasMany = array();
+		$this->autoSave = true;
 	}
 	
 	function getTableName()
@@ -64,6 +85,59 @@ class DbObject
 		return $this->scalars[$field];
 	}
 	
+	function setScalar($field, $value)
+	{
+		$data[$field] = $value;
+		$this->setScalars($data);
+	}
+	
+	function setScalars($data, $force = false)
+	{
+		$idFieldName = $this->getIdFieldName();
+		
+		//	if they passed in an id we don't need to reset it just verify that it is the right one
+		if(isset($data[$idFieldName]))
+		{
+			assert($data[$idFieldName] == $this->id);
+			unset($data[$idFieldName]);
+		}
+		
+		if($this->autoSave || $force)
+		{
+			$tableName = $this->getTableName();
+			
+			$this->assignScalars($data);
+			
+			$updateFields = array();
+			$updateValues = array();
+			foreach($data as $member => $value)
+			{
+				if($value === null)
+					$updateFields[] = "$member = NULL";
+				else
+				{
+					$updateFields[] = "$member = :$member";
+					$updateValues[$member] = $value;
+				}
+			}
+			$updateFields = implode(", ", $updateFields);
+			
+			SqlUpdateRow("update $tableName set $updateFields where $idFieldName = $this->id", $updateValues);
+		}
+		else
+		{
+			$this->assignScalars($data);
+		}
+	}
+	
+	function assignScalars($data)
+	{
+		foreach($data as $member => $value)
+		{
+			$this->scalars[$member] = $value;
+		}
+	}
+	
 	function loadScalars()
 	{
 		$tableName = $this->getTableName();
@@ -79,43 +153,37 @@ class DbObject
 		
 	}
 	
-	/*
-	function assignScalars($data)
+	function save()
 	{
-		foreach($data as $member => $value)
-		{
-			$this->scalars[$member] = $value;
-		}
-	}
-	*/
-	
-	/*
-	function getScalar($fieldName)
-	{
-		$tableName = $this->getTableName();
-		return SqlFetchCell("select $fieldName from person where id = :id", array('id' => $this->id));
-	}
-	*/
-	
-	//
-	//	orm
-	//
-	
-	function hasMany($className, $foreignKey = NULL)
-	{
-		if(!$foreignKey)
-			$foreignKey = $this->getTableName() . '_id';
-		$this->hasMany[$className] = $foreignKey;
+		echo_r($this->scalars);
+		$this->setScalars($this->scalars, true);
 	}
 	
-	function getMany($className)
+	function hasMany($name, $params = NULL)
 	{
-		//	hack to get around phps lack of decent static function handling
-		$dummy = new $className();
-		$tableName = $dummy->getTableName();
-		$fieldName = $this->hasMany[$className];
+		if(isset($params['class']))
+			$className = $params['class'];
+		else
+			$className = $name;
 		
-		$sql = "select * from $tableName where $fieldName = :id";
+		if(isset($params['field']))
+			$foreignKey = $params['field'];
+		else
+			$foreignKey = $this->getTableName() . '_id';
+		
+		$this->hasMany[$name] = array('className' => $className, 'foreignKey' => $foreignKey);
+	}
+	
+	function getMany($name)
+	{
+		$className = $this->hasMany[$name]['className'];
+		$foreignKey = $this->hasMany[$name]['foreignKey'];
+		
+		//	hack to get around phps lack of decent static function handling
+		$dummy = new $className('static');		
+		$tableName = $dummy->getTableName();
+		
+		$sql = "select * from $tableName where $foreignKey = :id";
 		$rows = SqlFetchRows($sql, array('id' => $this->id));
 		$objects = array();
 		foreach($rows as $thisRow)
@@ -124,5 +192,25 @@ class DbObject
 		}
 		
 		return $objects;
+	}
+	
+	function __get($varname)
+	{
+//		if(isset($this->$value))
+//			return $this->$value;
+		
+		if(isset($this->hasMany[$varname]))
+			return $this->getMany($varname);
+		
+		return $this->getScalar($varname);
+	}
+
+	function __set($varname, $value)
+	{
+//		if(isset($this->$varname))
+//			return;
+		
+		$this->autoSave = false;
+		$this->setScalar($varname, $value);
 	}
 }
