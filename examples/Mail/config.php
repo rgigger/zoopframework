b918e756763e0b19dad1da8ebec4584e3c7db643
rgigger <rgigger@585e9b84-f019-0410-b165-1d737e75257b>
2007-03-20 22:05:06 +0000

    added in an example to demonstrate the new mail module
    git-svn-id: https://zoop.googlecode.com/svn/trunk@111 585e9b84-f019-0410-b165-1d737e75257b

---
+++ examples/Mail/config.php
@@ -0,0 +1,6 @@
+<?php
+define('zoop_dir', '../../framework');
+define('app_dir', dirname(__file__));
+
+//	mail config
+define('mail_smtp_host', 'localhost');
---
+++ examples/Mail/index.php
@@ -0,0 +1,10 @@
+<?php
+include('config.php');
+include(zoop_dir . '/Zoop.php');
+
+Zoop::loadLib('gui');
+Zoop::loadLib('mail');
+
+$message = new Message();
+$message->assign('asdf', 'some text to include in the body of the email');
+$message->sendText('messages/test.tpl', 'example@localhost', 'Billy Joe', 'jim@dundermifflin.com', 'Jim Halpert', 'the subject of the email');
