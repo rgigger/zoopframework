5ee6fc27dd23a75fb4e6a750f53923429ff4be0d
rgigger <rgigger@585e9b84-f019-0410-b165-1d737e75257b>
2007-04-25 05:30:40 +0000

    added some migration stationary into the fitness project created some migrations in the fitness project
    git-svn-id: https://zoop.googlecode.com/svn/trunk@119 585e9b84-f019-0410-b165-1d737e75257b

---
+++ apps/fitness/migrations/00.00.01_start.php
@@ -0,0 +1,12 @@
+<?php
+class Migration_00_00_01 extends Migration
+{
+	function up()
+	{
+		SqlQuery();
+	}
+	
+	function down()
+	{
+	}
+}
---
+++ apps/fitness/stationary/migration.tpl
@@ -0,0 +1,11 @@
+<?php
+class Migration_[[$version]] extends Migration
+{
+	function up()
+	{	
+	}
+	
+	function down()
+	{
+	}
+}
