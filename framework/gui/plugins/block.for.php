4f5a736c3bf20b6cedbeba0af823235bfba872c1
rgigger <rgigger@585e9b84-f019-0410-b165-1d737e75257b>
2007-06-15 00:09:56 +0000

    allows you to name for loops so they can be nested
    git-svn-id: https://zoop.googlecode.com/svn/trunk@126 585e9b84-f019-0410-b165-1d737e75257b

--- framework/gui/plugins/block.for.php
+++ framework/gui/plugins/block.for.php
@@ -4,21 +4,22 @@ function smarty_block_for($params, $content, &$smarty, &$repeat)
 	$start = $params['start'];
 	$end = $params['end'];
 	$varname = $params['varname'];
+	$name = isset($params['name']) ? $params['name'] : '__default__';
 	
 	if($repeat)
 	{
-		$smarty->_for['__default__']['cur'] = $start;
+		$smarty->_for[$name]['cur'] = $start;
 	}
 	else
 	{
-		$smarty->_for['__default__']['cur']++;
+		$smarty->_for[$name]['cur']++;
 	}
 	
-	if($smarty->_for['__default__']['cur'] > $end)
+	if($smarty->_for[$name]['cur'] > $end)
 		$repeat = false;
 	else
 	{
-		$smarty->assign($varname, $smarty->_for['__default__']['cur']);
+		$smarty->assign($varname, $smarty->_for[$name]['cur']);
 		$repeat = true;
 	}
 	
