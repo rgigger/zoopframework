0f6d1b658b07befc6b0985a6ed8a7a8abcaec3d4
rgigger <rgigger@585e9b84-f019-0410-b165-1d737e75257b>
2008-02-29 20:46:08 +0000

    sql files for an example
    git-svn-id: https://zoop.googlecode.com/svn/trunk@179 585e9b84-f019-0410-b165-1d737e75257b

---
+++ examples/DbObject/sql/guid_person.sql
@@ -0,0 +1,10 @@
+begin;
+
+CREATE TABLE guid_person
+(
+	id text primary key,
+	firstname TEXT,
+	lastname TEXT
+);
+
+commit;
---
+++ examples/DbObject/sql/person_stuff.sql
@@ -0,0 +1,10 @@
+begin;
+
+CREATE TABLE person_stuff
+(
+	id INTEGER primary key autoincrement,
+	firstname TEXT,
+	lastname TEXT
+);
+
+commit;
