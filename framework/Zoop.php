d827cd2db4886e6a3e18ddb5906a73286e3020e0
taxilian <taxilian@585e9b84-f019-0410-b165-1d737e75257b>
2008-04-02 13:44:06 +0000

    Added phpdoc comments
    git-svn-id: https://zoop.googlecode.com/svn/trunk@218 585e9b84-f019-0410-b165-1d737e75257b

--- framework/Zoop.php
+++ framework/Zoop.php
@@ -6,10 +6,26 @@ include(zoop_dir . '/ZoopModule.php');
 //	we want to load this before we do anything else so that everything else is easier to debug
 include(zoop_dir . '/app/Error.php');
 
+/**
+ * This object is for basic framework management tasks, such as:
+ * * Configuring auto-loading of zones and modules
+ * * Loading libraries and domains
+ */
 class Zoop
 {
+	/**
+	 * Key => Value list of registered classes and the full path of the file that contains them
+	 *
+	 * @var array
+	 */
 	var $classList;
 	
+	/**
+	 * When a Zoop object is instantiated, the "utils" library is automatically
+	 * imported and the app_tmp_dir constant is defined (to projectdir/tmp)
+	 *
+	 * @return Zoop
+	 */
 	function Zoop()
 	{
 		$this->classList = array();
@@ -18,11 +34,24 @@ class Zoop
 		DefineOnce('app_tmp_dir', app_dir . '/tmp');
 	}
 	
+	/**
+	 * Register a class for auto-loading with the name of the class
+	 * and the full path of the file that contains it.
+	 *
+	 * @param string $className
+	 * @param string $fullPath
+	 */
 	function _registerClass($className, $fullPath)
 	{
 		$this->classList[strtolower($className)] = $fullPath;
 	}
 	
+	/**
+	 * Returns the full path and filename associated with the given registered class
+	 *
+	 * @param string $className
+	 * @return string - full path and filename of the class
+	 */
 	function _getClassPath($className)
 	{
 		$className = strtolower($className);
@@ -32,24 +61,50 @@ class Zoop
 		return false;
 	}
 	
+	/**
+	 * static -- Register a class for auto-loading with the name of the class
+	 * and the full path of the file that contains it.
+	 *
+	 * @param string $className
+	 * @param string $fullPath
+	 */
 	static function registerClass($className, $fullPath)
 	{
 		global $zoop;
 		$zoop->_registerClass($className, $fullPath);
 	}
 	
+	/**
+	 * Register a "domain" class for autoload (a domain class is a
+	 * class that is located in the "domains" directory under
+	 * the project root with the filename <classname>.php)
+	 *
+	 * @param unknown_type $className
+	 */
 	static public function registerDomain($className)
 	{
 		self::registerClass($className, app_dir . '/domain/' . $className . '.php');
 	}
 	
-	//	static - singleton
+	/**
+	 * static -- Returns the full path and filename associated with the given registered class
+	 *
+	 * @param string $className
+	 * @return string - full path and filename of the class
+	 */
 	function getClassPath($className)
 	{
 		global $zoop;
 		return $zoop->_getClassPath($className);
 	}
 	
+	/**
+	 * Loads a library of specified name.  Modules are located in the root
+	 * directory of the framework, using the following naming scheme:
+	 * 	<root>/<module>/<module>.php
+	 *
+	 * @param string $name
+	 */
 	function loadLib($name)
 	{
 		//	allow static calls to use the singleton object
@@ -77,6 +132,13 @@ class Zoop
 
 $zoop = new Zoop();
 
+/**
+ * Automatic class loading handler.  This automatically loads a class using the path
+ * information that was registered using the Zoop::registerClass or ::registerDomain
+ * method 
+ *
+ * @param string $className
+ */
 function __autoload($className)
 {
 	$classPath = Zoop::getClassPath($className);
