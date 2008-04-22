71982ab367e1a1ba444d5a8aece213823994e5bf
taxilian <taxilian@585e9b84-f019-0410-b165-1d737e75257b>
2008-04-22 15:51:37 +0000

    Added dummy implementations for the abstract functions in DbConnection to DbPdo so that the examples will work
    git-svn-id: https://zoop.googlecode.com/svn/trunk@234 585e9b84-f019-0410-b165-1d737e75257b

--- framework/db/DbPdo.php
+++ framework/db/DbPdo.php
@@ -5,23 +5,54 @@ class DbPdo extends DbConnection
 	{
 		if($this->params['file'][0] != '/')
 			$this->params['file'] = app_dir . '/var/' . $this->params['file'];
-		
+
 		$this->conn = new PDO('sqlite:' . $this->params['file']);
 		$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 		$this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_NAMED);
 	}
-	
+
 	function escapeString($string)
 	{
 		return $this->conn->quote($string);
 	}
-	
+
+		/**
+	 * Checks if a given table exists in the database
+	 *
+	 * @param string $name Name of the table to look for
+	 * @return boolean True if the table exists in this database
+	 */
+	public function tableExists($name)
+	{
+		trigger_error("tableExists method not yet implemented in DbPdo");
+	}
+
+	/**
+	 * Returns an array of table names that exist in the database
+	 *
+	 * @return array Array of table names
+	 */
+	public function getTableNames()
+	{
+		trigger_error("getTableNames is not yet implemented in DbPdo");
+	}
+
+	/**
+	 * Returns field information about the specified table
+	 *
+	 * @param string $tableName Name of the table to return information about
+	 */
+	public function getTableFieldInfo($tableName)
+	{
+		trigger_error("getTableFieldInfo is not yet implemented in DbPdo");
+	}
+
 	function _query($sql)
 	{
-		$result = $this->conn->query($sql);			
+		$result = $this->conn->query($sql);
 		return new DbPdoResult($this->connection, $result);
 	}
-	
+
 	function getLastInsertId()
 	{
 		return $this->conn->lastInsertId();
