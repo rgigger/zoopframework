c99fba2c643f1c440c98ae9d2e229cbf66cb705a
Rick Gigger <rgigger@gmail.com>
2009-02-16 23:00:02 -0700

    minor update to the session stuff

--- framework/session/Session.php
+++ framework/session/Session.php
@@ -1,32 +1,32 @@
 <?php
 class Session
 {
-	function start()
+	static function start()
 	{
 		SessionModule::getEngine()->start();
 	}
 	
-	function get($key = '__default__')
+	static function get($key = '__default__')
 	{
 		SessionModule::getEngine()->get($key);
 	}
 	
-	function getWithLock($key = '__default__')
+	static function getWithLock($key = '__default__')
 	{
 		SessionModule::getEngine()->getWithLock($key);
 	}
 	
-	function set($value, $key = '__default__')
+	static function set($value, $key = '__default__')
 	{
 		SessionModule::getEngine()->set($value, $key);
 	}
 	
-	function saveChangesUnsafe()
+	static function saveChangesUnsafe()
 	{
 		SessionModule::getEngine()->saveChangesUnsafe();
 	}
 	
-	public function destroy()
+	static public function destroy()
 	{
 		// Unset all of the session variables.
 		$_SESSION = array();
@@ -37,5 +37,10 @@ class Session
 
 		// Finally, destroy the session.
 		session_destroy();		
+	}
+	
+	static public function getId()
+	{
+		return session_id();
 	}
 }
--- framework/session/init.sql
+++ framework/session/init.sql
@@ -1,5 +1,3 @@
--- postgresql
-
 begin;
 
 CREATE TABLE session_base
@@ -13,27 +11,7 @@ CREATE TABLE session_data
 	session_id text NOT NULL,
 	key text,
 	value text,
-	CONSTRAINT session_data_pkey PRIMARY KEY (session_id, key)
-);
-
-commit;
-
-
--- sqlite
-
-begin;
-
-CREATE TABLE session_base
-(
-	session_id text NOT NULL PRIMARY KEY,
-	last_active timestamp with time zone NOT NULL
-);
-
-CREATE TABLE session_data
-(
-	session_id text NOT NULL,
-	key text,
-	value text
+	PRIMARY KEY (session_id, key)
 );
 
 commit;
