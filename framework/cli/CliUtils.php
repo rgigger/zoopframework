cbb2274a38d1731a8289a1a36d275205e459914b
rgigger <rgigger@585e9b84-f019-0410-b165-1d737e75257b>
2009-02-18 08:23:42 +0000

    not actually using this file yet
    git-svn-id: https://zoop.googlecode.com/svn/trunk@299 585e9b84-f019-0410-b165-1d737e75257b

---
+++ framework/cli/CliUtils.php
@@ -0,0 +1,18 @@
+<?php
+// class CliUtils
+// {
+// 	static function preParseParams($params)
+// 	{
+// 		$shortFlags = array();
+// 		$longFlags = arrray();
+// 		$flags = array();
+// 		$nonFlags = array();
+// 		$cur = 0;
+// 		foreach($params as $thisParam)
+// 		{
+// 			
+// 		}
+// 		
+// 		$allParams = $params;
+// 	}
+// }
